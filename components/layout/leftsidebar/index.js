b152f7a74c06f389feeab21f5a3575ab72631b70
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-03-30 14:33:01 +0530

    left side bar created

--- components/layout/leftsidebar/index.js
+++ components/layout/leftsidebar/index.js
@@ -1,6 +1,7 @@
 import Image from "next/legacy/image";
 import Link from "next/link";
-
+import { FaFacebookF ,FaInstagram} from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
 export default function index() {
   return (
     <div className="leftsidebar">
@@ -32,16 +33,55 @@ export default function index() {
 </div>
 {/* home */}
   <li>
-  <div className="line"><Link href="/"> <hr /></Link></div>
+  <div className="line active"><Link href="/"> <hr /></Link></div>
   <div className="text"><Link href="/">HOME</Link></div>
   </li>
   {/* services */}
   <li>
-  <div className="line" style={{width:"53%"}}><hr /></div>
-  <div className="text">OUR SERVICES</div>
+  <div className="line" style={{width:"53%"}}><Link href="/"> <hr /></Link></div>
+  <div className="text"><Link href="/">OUR SERVICES</Link></div>
+  </li>
+
+   {/* ABOUT US */}
+   <li>
+  <div className="line" style={{width:"62%"}}><Link href="/"> <hr /></Link></div>
+  <div className="text"><Link href="/">ABOUT US</Link></div>
+  </li>
+
+  {/* ABOUT US */}
+  <li>
+  <div className="line" style={{width:"63%"}}><Link href="/"> <hr /></Link></div>
+  <div className="text"><Link href="/">CONTACT</Link></div>
+  </li>
+
+  {/* LOCATION */}
+  <li>
+  <div className="line"><Link href="/"> <hr /></Link></div>
+  <div className="text"><Link href="/">TIME</Link></div>
   </li>
 </div>
 </div>
+
+
+
+
+{/* social media links */}
+
+<div className="bottomIcons">
+
+  <div className="icons">
+  <Link href="https://www.facebook.com/beautywithbandana/"><li title="Facebook"><i><FaFacebookF /></i></li></Link>
+
+<Link href="https://www.instagram.com/beautywithbandhana/"><li title="Instagram"><i><FaInstagram/></i></li></Link>
+
+<Link href="mailto: Bandhana@beautywithb.com.au"><li title="Send Message"><i><CiMail/></i></li></Link>
+
+  </div>
+  <div className="rightLine">
+<hr />
+  </div>
+</div>
+
     </div>
   )
 }
