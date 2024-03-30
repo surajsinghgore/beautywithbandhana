c39dda8b67a045d1cf034e0ed7aebb5bc06af07d
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-03-30 16:09:02 +0530

    service page created

--- app/services/page.js
+++ app/services/page.js
@@ -8,7 +8,196 @@ export default function Home() {
    
 <h2>Our Services</h2>
 
+<div className={styles.services}>
 
+
+{/* card 1 massage*/}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/1.jpg" alt="service 1" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Massage</h4>
+    <p>Relax and unwind with our professional massage therapy. Soothe muscles, release tension, and rejuvenate your body and mind in a tranquil setting. Book your session now.</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+
+
+{/* card 2 skin care*/}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/2.jpg" alt="service 2" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Skin care</h4>
+    <p>Revitalize your skin with our luxurious skincare treatments. Achieve a radiant complexion and youthful glow with our expertly crafted products and personalized care routines.</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 3 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/3.jpg" alt="service 3" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Eyebrows & eyelashes</h4>
+    <p>Enhance your beauty with perfectly groomed eyebrows and luscious eyelashes. Our expert services provide precise shaping and extensions for captivating eyes that steal the spotlight.</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 4 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/4.webp" alt="service 4" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Men&rsquo;s Waxing</h4>
+    <p>Achieve smooth, long-lasting results with our specialized men&rsquo;s waxing services. Tailored for every grooming need, our professional treatments ensure a comfortable and confident experience.</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+{/* card 5 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/5.jpg" alt="service 5" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Women Waxing</h4>
+    <p>Experience silky-smooth skin with our expert women&rsquo;s waxing services. From brows to legs, our professional treatments ensure effective hair removal for a flawless finish that lasts.</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+{/* card 6 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/6.jpg" alt="service 6" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Women Waxing Package</h4>
+    <p>Indulge in our women&rsquo;s waxing package for a complete grooming experience. From brows to bikini line, enjoy smooth, hair-free skin with our professional treatments tailored to perfection.
+
+
+
+
+
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 7 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/7.webp" alt="service 7" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Tinting</h4>
+    <p>Enhance your natural beauty with our tinting services. From eyebrows to eyelashes, achieve defined and intensified features with our professional tinting treatments for a stunning, long-lasting look.
+
+
+
+
+
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+{/* card 8 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/8.webp" alt="service 8" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Lash Lift</h4>
+    <p>Elevate your look with our lash lift service. Achieve beautifully curled and lifted lashes that enhance your natural beauty, giving you a stunning and effortless allure.
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 9 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/9.jpg" alt="service9" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Threading </h4>
+    <p>
+Refine your look with our threading service. Experience precise hair removal that shapes and defines your brows and facial features, revealing your natural beauty with precision and elegance.
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 10 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/10.png" alt="service 10" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Facial Peel</h4>
+    <p>
+    Renew your skin with our facial peel service. Experience the rejuvenating effects of gentle exfoliation, revealing smoother, brighter, and more radiant skin for a refreshed and youthful complexion.
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+
+{/* card 11 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/11.webp" alt="service 11" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Microdermabration</h4>
+    <p>
+    Revitalize your skin with our microdermabrasion treatment. Experience the benefits of gentle exfoliation, reducing fine lines, acne scars, and uneven texture for a smoother, brighter complexion.
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+
+{/* card 12 */}
+<div className={styles.card}>
+<div className={styles.cardImage}>
+<Image src="/services/12.jpg" alt="service 12" layout="fill" priority/>
+</div>
+
+<div className={styles.details}>
+    <h4>Massage Packages
+</h4>
+    <p>
+    Indulge in relaxation with our massage packages. Choose from a variety of options tailored to your needs, offering ultimate tranquility and rejuvenation for body and mind.
+</p>
+    <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?menu=true"><button>Book Now</button></Link>
+</div>
+</div>
+</div>
 
 
    {/* footer */}
