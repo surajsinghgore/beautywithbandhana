ed16cfaba2079fff8f21282872ab6b89a88f3ccd
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-03-30 15:02:51 +0530

    home right top card created

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
    
+   <div className={styles.topHomeSection}>
+<div className={styles.bookOnline}>
+  <h1>Welcome to Beauty With Bandhana</h1>
+  <p>Experience our most potent and rapid peel,utilizing a unique three-acid approach for personalized deep exfoliation.</p>
+  
+  <Link href="https://www.fresha.com/a/beauty-with-bandhana-morley-289-benara-road-kixb51p0/booking?employeeId=2807218"><button>BOOK ONLINE SERVICES</button></Link>
+</div>
+   </div>
     </main>
   );
 }
