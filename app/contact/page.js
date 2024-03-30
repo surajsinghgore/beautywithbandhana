a77673d111a75da5c294a2845c3334a8c107321b
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-03-30 16:44:49 +0530

    footer border top added

--- app/contact/page.js
+++ app/contact/page.js
@@ -10,7 +10,7 @@ export default function Home() {
 <h2>Contact</h2>
 
 <div className={styles.map}>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3388.155280339509!2d115.9256708!3d-31.875194200000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32b1380f788bff%3A0x7dc4c8363d5b4c09!2sBeautywithbandhana!5e0!3m2!1sen!2sin!4v1711795304706!5m2!1sen!2sin"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3388.155280339509!2d115.9256708!3d-31.875194200000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32b1380f788bff%3A0x7dc4c8363d5b4c09!2sBeautywithbandhana!5e0!3m2!1sen!2sin!4v1711795304706!5m2!1sen!2sin"   referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 
 
